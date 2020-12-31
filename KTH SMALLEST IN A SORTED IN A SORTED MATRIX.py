b30d5b0b9418c31663ea4a267e4836b0a3fed1ac
Raj singh rajpoot <rajsinghrajpoot263@gmail.com>
2020-12-31 20:06:47 +0530

    Add files via upload

---
+++ KTH SMALLEST IN A SORTED IN A SORTED MATRIX.py
@@ -0,0 +1,31 @@
+'''
+Kth Smallest Element in a Sorted Matrix
+
+Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+Example:
+
+matrix = [
+   [ 1,  5,  9],
+   [10, 11, 13],
+   [12, 13, 15]
+],
+k = 8,
+
+return 13.
+
+'''
+import heapq
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        max_heap = []
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                heapq.heappush(max_heap,-1*matrix[i][j])
+                if len(max_heap)>k:
+                    heapq.heappop(max_heap)
+                
+        return -1*heapq.heappop(max_heap)
+        
