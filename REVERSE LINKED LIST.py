539dc882a5e3a6e0a8d9cacdf8486c7f4d305612
Raj singh rajpoot <rajsinghrajpoot263@gmail.com>
2021-01-08 17:05:28 +0530

    Add files via upload

---
+++ REVERSE LINKED LIST.py
@@ -0,0 +1,40 @@
+'''
+
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+'''
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        curr = head
+        prev = None
+        
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        
+        head = prev
+        return head
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        
+        def reverse(head):
+            if head is None or head.next is None:
+                return head
+            
+            tmp = reverse(head.next)
+            head.next.next = head
+            head.next = None
+            
+            return tmp
+        return reverse(head)
+
+
